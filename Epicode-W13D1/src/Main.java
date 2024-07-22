10baaf5b9d08fd73cb47203eb276cc9c29c874db
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-22 13:51:08 +0200

    Initial commit

---
+++ Epicode-W13D1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(5, 3);
+        System.out.println(result);
+    }
+}
